59840c5ad08b3cb8f41f40987878ca862a2b55cd
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-12 10:37:33 +0100

    Add files via upload

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+
+    var myRes = 75;
+    // only change  code below this 
+    
+      myRes++;
+      var myRes = 75;
+      myRes = myRes+1;
+
+
+    // only change code above this line 
+
+    return myRes;
+}
+console.log (main());
+module.exports = main;
