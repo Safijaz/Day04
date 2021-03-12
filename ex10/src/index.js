514c564629270613a91ae52ed92686b1ac44b113
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-12 10:41:34 +0100

    Add files via upload

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    //only change code below this line
+      
+      var myFirstName = "Gayatri";
+      var myLastName = "Spivak";
+      var myHomeTown = "Kolkata";
+
+    // only change code above this line 
+    return {
+
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+
+console.log(main());
+module.exports = main;
